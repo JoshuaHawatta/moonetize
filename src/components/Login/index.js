f32aff3b8274aac46002bfdc615c9a09a3782a62
JoshuaHawatta <joshuahawattaazevedo@gmail.com>
2022-05-13 15:24:35 -0300

    using context-api to save the data that the user put on the inputs

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,12 +1,40 @@
-import MoonetizeJSX from '../../pages/MoonetizeHub';
-import CheckHasUser from './CheckHasUser';
+import { memo, useContext } from 'react';
+import BrlCurrencyComponent from '../../currency';
+import { LoggedUserContext } from '../../contexts/Logged-user';
+import './login.css';
 
-const SignOrLogin = () => {
-  if(localStorage.length < 2) {
-    return( <CheckHasUser /> )
+const LoginPage = () => {
+  const { setUserIsLogged, userName, setUserName, userMoney } = useContext(LoggedUserContext);
+
+  const logUser = () => {
+    localStorage.setItem('User', JSON.stringify({ userName, userMoney} ));
+    setUserIsLogged(true);
   }
-  
-  return( <MoonetizeJSX /> )
+
+  return(
+    <section className="cover">
+      <h2 className="headline-2 text-primary-color">Antes de Começar...</h2>
+      
+      <div className="field-group">
+        <div className='field'>
+          <label className='labels' htmlFor='nameInput'>Me fala seu nome!</label>
+          <input 
+            type='text' 
+            placeholder='Exemplo: Zé da silva'  
+            onChange={ e => setUserName(e.target.value) } 
+            value={ userName } 
+            id='nameInput' />
+        </div>
+
+        <div className='field'>
+          <label className='labels' htmlFor='moneyInput'>Você vai gastar quanto?</label>
+            <BrlCurrencyComponent placeholder='R$' id='moneyInput' />
+        </div>
+
+        <button className="btn" onClick={ logUser }>Vamos, Começar!</button>
+      </div>
+    </section>
+  )
 }
 
-export default SignOrLogin;
+export default memo(LoginPage);
