7c8158c0f30dadf3205c567ec7c5dceb3001aab0
JoshuaHawatta <joshuahawattaazevedo@gmail.com>
2022-05-13 15:28:56 -0300

    changed MoonetizeHub page name to Home

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import AboutJSX from './pages/About';
-import SignOrLogin from './components/Login';
+import HomeJSX from './pages/Home';
 import CartJSX from './pages/Cart';
 import PassTheHatJSX from './pages/PassTheHat';
 
 const PAGES_URLS = () => {
   return(
     <Routes>
-      <Route path='/' element={ <SignOrLogin /> } />
+      <Route path='/' element={ <HomeJSX /> } />
       <Route path='/pass-the-hat' element={ <PassTheHatJSX /> } />
       <Route path='/cart' element={ <CartJSX /> } />
       <Route path='/about' element={ <AboutJSX /> } />
