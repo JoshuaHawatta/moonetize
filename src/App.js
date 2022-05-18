809af4e47e404834cb140bf6914b8bdeb160b259
JoshuaHawatta <joshuahawattaazevedo@gmail.com>
2022-05-18 01:47:40 -0300

    using new context on all application

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import PAGES_URLS from './routes';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import LoggedUserProvider from './contexts/Logged-user';
+import ShowMoneyAmmountProvider from './contexts/ShowMoney';
 import './setup.css';
 
 const App = () => {
   return(
     <LoggedUserProvider>
-      <Router>
-        <Header />
-        <main className="App">    
-          <PAGES_URLS />
-        </main>
-        <Footer />
-      </Router>
+      <ShowMoneyAmmountProvider>
+        <Router>
+          <Header />
+          <main className="App">    
+            <PAGES_URLS />
+          </main>
+          <Footer />
+        </Router>
+      </ShowMoneyAmmountProvider>
     </LoggedUserProvider>
   )
 }
