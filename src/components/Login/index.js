0116b1b8f599a09408fb75c98dcdb3bf7cd40367
JoshuaHawatta <joshuahawattaazevedo@gmail.com>
2022-05-18 01:50:49 -0300

    added regex to the login page and changed local to sessionStorage

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,29 +1,38 @@
-import { memo, useContext } from 'react';
+import { memo, useState, useContext } from 'react';
 import BrlCurrencyComponent from '../../currency';
 import { LoggedUserContext } from '../../contexts/Logged-user';
+import { nameRegex } from './regex.js';
 import './login.css';
 
 const LoginPage = () => {
-  const { setUserIsLogged, userName, setUserName, userMoney } = useContext(LoggedUserContext);
+  const [errorMessage, setErrorMessage] = useState('');
+  const { setUserIsLogged, userName, setUserName } = useContext(LoggedUserContext);
 
   const logUser = () => {
-    localStorage.setItem('User', JSON.stringify({ userName, userMoney} ));
-    setUserIsLogged(true);
+    if(nameRegex.test(userName)) {
+      sessionStorage.setItem('User', JSON.stringify(userName));
+      setUserIsLogged(true);
+    }else {
+      setErrorMessage('Você precisa de no mínimo 2 caractéres para fazer o login!');
+      return
+    }
   }
 
   return(
-    <section className="cover">
-      <h2 className="headline-2 text-primary-color">Antes de Começar...</h2>
-      
+    <section className='login-section'>
+      <h2>Antes de Começar...</h2>
       <div className="field-group">
+
         <div className='field'>
           <label className='labels' htmlFor='nameInput'>Me fala seu nome!</label>
           <input 
-            type='text' 
-            placeholder='Exemplo: Zé da silva'  
-            onChange={ e => setUserName(e.target.value) } 
-            value={ userName } 
+            type='text'
+            autoComplete='off'
+            placeholder='Exemplo: Zé da silva'
+            onChange={ e => setUserName(e.target.value) }
+            value={ userName }
             id='nameInput' />
+            <span className='errorMessage'>{ errorMessage }</span>
         </div>
 
         <div className='field'>
@@ -31,7 +40,7 @@ const LoginPage = () => {
             <BrlCurrencyComponent placeholder='R$' id='moneyInput' />
         </div>
 
-        <button className="btn" onClick={ logUser }>Vamos, Começar!</button>
+        <button className="btn" onClick={ logUser }>Vamos Começar!</button>
       </div>
     </section>
   )
