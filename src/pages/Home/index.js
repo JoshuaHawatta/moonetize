d19225988a7f923fa5fb4070116da2e8b8f86f2d
JoshuaHawatta <joshuahawattaazevedo@gmail.com>
2022-05-18 01:54:57 -0300

    created Home page structure and used the showMoney context on it

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,58 @@
-import { useContext } from 'react';
+import { useContext, memo } from 'react';
+import { Link } from 'react-router-dom';
 import { LoggedUserContext } from '../../contexts/Logged-user';
+import { ShowMoneyAmmountContext } from '../../contexts/ShowMoney';
 import LoginPage from '../../components/Login';
+import { FaMoneyCheckAlt, FaShoppingCart, FaEye, FaEyeSlash } from 'react-icons/fa';
+import './home.css';
 
 const HomeJSX = () => {
-  const { userIsLogged } = useContext(LoggedUserContext);
+  const userNameData = JSON.parse(sessionStorage.getItem('User'));
+  const { userIsLogged, userMoney } = useContext(LoggedUserContext);
+  const { showMoneyAmmount, setShowMoneyAmmount } = useContext(ShowMoneyAmmountContext);
 
-  if(!userIsLogged && localStorage.length === 0) {
+  if(!userIsLogged && sessionStorage.length === 0) {
     return(
       <LoginPage /> 
     )
-  }
+  }else {
+    return (
+      <main>
+        <div className='user-hub-div'>
+          <div className='user-data'>
+            <h1>Olá, { userNameData }!</h1>
+            <p>{ showMoneyAmmount ? userMoney[1] : '****' }</p>
+          </div>
+  
+          <div className='hide-or-show-money-icon'>
+            { showMoneyAmmount 
+              ? <FaEye className='eye-icon open' onClick={ () => setShowMoneyAmmount(false) } /> 
+              : <FaEyeSlash className='eye-icon shut' onClick={ () => setShowMoneyAmmount(true) } /> 
+            }
+          </div>
+        </div>
 
-  return (
-    <h1>Você me logou!</h1>
-  )
+        <div className='functionalities'>
+          <div className='functionality'>
+            <Link to='/pass-the-hat'>
+              <FaMoneyCheckAlt />
+              <h2>Vaquinha</h2>
+              <span>Ver quanto preciso dar da minha parte.</span>
+            </Link>
+          </div>
+  
+          <div className='functionality'>
+            <Link to='cart'>
+              <FaShoppingCart />
+              <h2>Carrinho</h2>
+              <span>Ver quanto preciso já gastei.</span>
+            </Link>
+          </div>
+
+        </div>
+      </main>
+    )
+  }
 }
 
-export default HomeJSX;
+export default memo(HomeJSX);
