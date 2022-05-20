19862f7122ca536ab5c9fe180a3eb8175de110d6
JoshuaHawatta <joshuahawattaazevedo@gmail.com>
2022-05-19 23:00:35 -0300

    created the styled components to the Cart page

---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const CartSection = styled.section `
+  background-color: red;
+`;
+
+export const AddItemDiv = styled.div `
+  background-color: blue;
+`;
+
+export const ProductInputDiv = styled.div `
+  background-color: green;
+`;
+
+export const TotalItemsDiv = styled.div `
+  background-color: brown;
+`;
+
+export const EndPurchaseDiv = styled.div `
+  background-color: pink;
+`;
