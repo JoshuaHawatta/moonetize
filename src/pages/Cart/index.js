749fc597a19defe0e6bf9e6cc97f1ed827015aef
JoshuaHawatta <joshuahawattaazevedo@gmail.com>
2022-05-24 18:46:10 -0300

    implemented an end purchase logic, removed itemQuantity and implemented some logic to save the the user money minus the buy price

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,95 +1,63 @@
 import { useState, useCallback, memo, useContext } from 'react';
 import { AiFillDelete } from 'react-icons/ai';
 import { LoggedUserContext } from '../../contexts/Logged-user';
-import { CartSection, AddDiv, ProductInputDiv, TotalItemsDiv, EndPurchaseDiv } from './styles.js';
+import { CartSection, AddDiv, ProductInputDiv, TotalItemsDiv } from './styles.js';
+
+const convertStringToNumber = string => Number(string.replace(',', '.'))
 
 const CartJSX = () => {
+  const { userMoney, setUserMoney } = useContext(LoggedUserContext);
   const [inputValues, setInputValues] = useState({});
   const [cartItems, setCartItems] = useState([]);
-  const [endPurchase, setEndPurchase] = useState(false);
-  const { setUserMoney, userMoney } = useContext(LoggedUserContext);
+  const [endPurchase, setEndpurchase] = useState(false);
 
   const handleInputChange = useCallback(e => {
     const { name, value } = e.target;
-    setInputValues({ ...inputValues, [name]: value })
+
+    setInputValues({...inputValues, [name]: value})
   }, [inputValues])
 
-  const handleAdd = useCallback(item => {
+  const handleAdd = useCallback(({ name, productPrice }) => {
     setCartItems(prevState => [...prevState, 
       {
-        name: item.name,
-        productPrice: item.productPrice,
-        quantity: item.quantity,
-        id: Date.now(),
-      }])
-
+        name: name, 
+        productPrice: 
+        productPrice, id: Date.now()
+      }]
+    )
     setInputValues('');
-  }, [cartItems])
+  }, [cartItems, inputValues])
 
   const handleRemove = useCallback(id => {
     setCartItems(prevState => prevState.filter(item => item.id !== id));
   }, [cartItems])
 
-  const ShowCartProducts = () => {
-    const productsInCart = cartItems.map((item, index) => {
-      const { name, id, productPrice, quantity } = item;
-      const WHOLE_PRICE = Number(productPrice.replace(',', '.')) * quantity;
+  const totalPrice = cartItems.reduce((acumulator, actualItem) => {
+    return acumulator + convertStringToNumber(actualItem.productPrice);
+  }, 0);
 
-      return(
-        <div key={ id }>
-          <p>{index +1}</p>
-          <h3>{ name }</h3>
-          <p>Valor: R${ productPrice } Quant.({ quantity }) Total: R${ WHOLE_PRICE.toFixed(2) }</p>
-          <button onClick={() => handleRemove(id) }><AiFillDelete /></button>
-        </div>
-      )
-    })
-
-    return (
-      <>
-        { productsInCart }
-      </>
-    )
-  }
-
-  const ShowPurchasePrice = () => {
-    const cartTotalPrice = cartItems
-      .map(item => Number(item.productPrice.replace(',', '.') * item.quantity))
-      .reduce((acumulator, actualValue) => acumulator + actualValue, 0);
-
-    const showFullPrice = cartTotalPrice.toFixed(2).replace('.', ',');
-
-    return (
-      <span>preço final da compra:R${ showFullPrice }</span>
-    )
-  }
-
-  const HandleEndPurchase = useCallback(() => {
-    const cartTotalPrice = cartItems
-      .map(item => Number(item.productPrice.replace(',', '.') * item.quantity))
-      .reduce((acumulator, actualValue) => acumulator + actualValue, 0);
-
-    setUserMoney(prevState => [...prevState, userMoney[0] - cartTotalPrice])
-  }, [userMoney])
+  const handleEndPurchase = () => {
+    setUserMoney(userMoney - totalPrice);
+    setEndpurchase(true);
+  };
 
   return(
     <CartSection>
       <h1>Carrinho</h1>
+      <h2>Dinheiro restante: R${ userMoney.toFixed(2) }</h2>
+      { cartItems.length === 0 ? <></> : <h2>Valor da compra: R${ totalPrice.toFixed(2) }</h2> }
+
       <AddDiv>
         <ProductInputDiv>
-          <input 
+          <label htmlFor='product-name'>Produto</label>
+          <input
+            id='product-name'
             type='text'
             name='name'
             onChange={ handleInputChange }
             value={ inputValues.name || '' }
             autoComplete='off'
             placeholder='Doritos' />
-
-          <input type='number' 
-            min='1' 
-            name='quantity' 
-            value={ inputValues.quantity || '' }
-            onChange={ handleInputChange } />
 
           <label htmlFor='product-price'>R$</label>
           <input 
@@ -106,12 +74,29 @@ const CartJSX = () => {
       </AddDiv>
 
       <TotalItemsDiv>
-        <ShowCartProducts />
-
-        <EndPurchaseDiv>
-          <ShowPurchasePrice />
-        </EndPurchaseDiv>
+        {
+          cartItems.map((item, index) => {
+            const { name, id, productPrice } = item;
+            const wholePrice = convertStringToNumber(item.productPrice);
+        
+            return(
+              <div key={ id }>
+                <span>{ index +1 }</span>
+                <h3>{ name }</h3>
+                <p>Valor: R${ productPrice } Total: R${ wholePrice.toFixed(2).replace('.', ',') }</p>
+                <button onClick={() => handleRemove(id) }><AiFillDelete /></button>
+              </div>
+            )
+          })
+        }
       </TotalItemsDiv>
+
+      <div>
+      { cartItems.length === 0 || endPurchase === true 
+        ? <></> 
+        : <button onClick={() => handleEndPurchase()}>Encerrar compra</button> }        
+      </div>
+
     </CartSection>
   )
 }
