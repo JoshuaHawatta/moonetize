9972482a4f9c4f89c541c3e8263f1c7a9e4553e8
JoshuaHawatta <joshuahawattaazevedo@gmail.com>
2022-05-24 23:03:00 -0300

    erased some ContextApi usless files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './setup.css';
 
 //COMPONENTS_AND_ROUTES
 import PAGES_URLS from './routes';
@@ -8,22 +7,22 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 
 //PROVIDERS 
-import LoggedUserProvider from './contexts/Logged-user';
+import LoggedUserProvider from './contexts/LoggedUser';
 import ShowMoneyAmmountProvider from './contexts/ShowMoney';
-import ShowOrHideHeaderProvider from './contexts/DisplayHeader';
+
+import './setup.css';
 
 const App = () => {
+
   return(
-    <LoggedUserProvider>
-      <ShowOrHideHeaderProvider>
-        <ShowMoneyAmmountProvider>
-          <Router>
-            <Header />
-            <PAGES_URLS />
-            <Footer />
-          </Router>
-        </ShowMoneyAmmountProvider>
-      </ShowOrHideHeaderProvider>
+    <LoggedUserProvider>    
+      <ShowMoneyAmmountProvider>
+        <Router>
+          <Header />
+          <PAGES_URLS />
+          <Footer />
+        </Router>
+      </ShowMoneyAmmountProvider>
     </LoggedUserProvider>  
   )
 }
