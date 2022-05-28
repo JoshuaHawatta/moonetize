4679bbdaef689372c2bcfe69bb02efe7e4da358e
JoshuaHawatta <joshuahawattaazevedo@gmail.com>
2022-05-28 17:05:20 -0300

    implemented ErrorMessageContext at the login page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,16 +1,18 @@
-import { memo, useState, useContext, useCallback } from 'react';
+import { memo, useContext, useCallback } from 'react';
 import { nameRegex } from './regex.js';
 import { BRL_LOGIN_CURRENCY } from '../../currency';
 import { LoggedUserContext } from '../../contexts/LoggedUser';
+import { ErrorMessagesContext } from '../../contexts/ErrorMessage';
 import { LoginSection, InputFieldDiv, Field } from './styles.js';
 
 const LoginPage = () => {
-  const [errorMessage, setErrorMessage] = useState('');
   const { setLoggedUser, userName, setUserName, userMoney } = useContext(LoggedUserContext);
+  const { errorMessage, setErrorMessage } = useContext(ErrorMessagesContext);
 
   const handleLogUser = useCallback(() => {
     if(nameRegex.test(userName)) {
       setLoggedUser(true);
+      setErrorMessage('');
       sessionStorage.setItem('User', JSON.stringify({ userName, userMoney }));
     }else {
       setErrorMessage('Você precisa de no mínimo 1 caractér para fazer o login!');
