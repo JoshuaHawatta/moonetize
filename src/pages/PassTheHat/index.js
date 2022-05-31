7c4fb82185f198de8e15918555f79ed99d66cc9c
JoshuaHawatta <joshuahawattaazevedo@gmail.com>
2022-05-30 22:36:38 -0300

    created a "coming soon" warning at Pass The Hat page

--- src/pages/PassTheHat/index.js
+++ src/pages/PassTheHat/index.js
@@ -1,8 +1,10 @@
+import { Temporarydiv } from './styles';
+
 const PassTheHatJSX = () => {
   return(
-    <>
+    <Temporarydiv>
       <h1>Página da Vaquinha em desenvolvimento!</h1>
-    </>
+    </Temporarydiv>
   )
 }
   
---
+++ src/pages/PassTheHat/styles.js
@@ -0,0 +1,10 @@
+export const TemporaryDiv = styled.div `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 70vh;
+  margin: 0 auto;
+
+  h1 { font: 5rem 'Overpass', Arial, sans-serif; }
+`;
